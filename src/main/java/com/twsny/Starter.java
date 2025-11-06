4fe8e70819cd3f9cbaa31d15a81f465b69f0d6ca
wanghf <wanghf@jousing.com>
2025-11-06 15:06:08 +0800

    third commit

--- src/main/java/com/twsny/Starter.java
+++ src/main/java/com/twsny/Starter.java
@@ -2,6 +2,6 @@ package com.twsny;
 
 public class Starter {
     public static void main(String[] args) {
-        System.out.println("HELLO TWSNY");
+        System.out.println("HELLO TWSNY!");
     }
 }
